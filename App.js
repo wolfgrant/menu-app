575ed3248aaa50ef824ffaa78f703ca692b76a48
wolfgrant <caman.developer@gmail.com>
2024-02-06 09:29:35 -0500

    feat: styling meal item

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { StyleSheet } from 'react-native';
 import CategoriesScreen from './screens/CategoryScreen';
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import MealsOverviewScreen from './components/MealsOverviewScreen';
+import MealsOverviewScreen from './screens/MealsOverviewScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,7 +13,7 @@ export default function App() {
     <>
       <StatusBar style='dark' />
       <NavigationContainer>
-        <Stack.Navigator>
+        <Stack.Navigator initialRouteName='MealsCategories'>
           <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
           <Stack.Screen name='MealsOverview' component={MealsOverviewScreen} />
         </Stack.Navigator>
--- screens/CategoryScreen.js
+++ screens/CategoryScreen.js
@@ -1,18 +1,29 @@
-import CategoryGridTile from '../components/CategoryGridTile';
+import CategoryGridTile from '../components/CategoryGridTile'
 import { CATEGORIES } from '../data/dummy-data'
 import { FlatList } from 'react-native'
 
-function renderCategoryItem(itemData) {
-    return <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />;
-}
+export default function CategoriesScreen({ navigation }) {
+  function renderCategoryItem(itemData) {
+    function pressHandle() {
+        navigation.navigate('MealsOverview', {
+            categoryId: itemData.item.id
+        });
+    }
 
-export default function CategoriesScreen() {
     return (
-        <FlatList
-            data={CATEGORIES}
-            keyExtractor={(item) => item.id}
-            renderItem={renderCategoryItem}
-            numColumns={2}
-        />
+      <CategoryGridTile
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onPress={pressHandle}
+      />
     )
+  }
+  return (
+    <FlatList
+      data={CATEGORIES}
+      keyExtractor={(item) => item.id}
+      renderItem={renderCategoryItem.bind(navigation)}
+      numColumns={2}
+    />
+  )
 }
